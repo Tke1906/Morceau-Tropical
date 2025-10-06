6642d093469600ee1349e2dadd22ad03c5485171
Takeshi <kanatsutakeshi19@gmail.com>
2025-10-06 18:31:50 +0900

    Ta Começando a funcionar

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -1,19 +1,38 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+
 
 public class GridManager : MonoBehaviour
 {
     [SerializeField] private int _width, _height;
-    [SerializeField] private Tile _preFab;
+    [SerializeField] private Tiles _preFab;
+    [SerializeField] private Transform _camera;
+    void Start()
+    {
+
+        createGrid();
+    }
+
+
 
     void createGrid()
     {
         for (int x = 0; x < _width; x++)
         {
             for (int y = 0; y < _height; y++)
-        {
-                transform.Rotate(Vector3.up);
+            {
+                // Instatiate(O que será instanciado, Onde será instanciado e para não mexer no eixo z)
+                var spawnedTile = Instantiate(_preFab, new Vector2(x, y), Quaternion.identity);
+
+                spawnedTile.name = $"Tile{x}:{y}";
+
+                var isDifferent = (x % 2 != y % 2);
+                spawnedTile.SetColor(isDifferent);
+            }
         }
-        }
+        _camera.position = new Vector3((float)_width / 2 - 0.5f, (float)_height / 2 - 0.5f, -10);
 
     }
 }
--- Assets/Scripts/Tiles.cs
+++ Assets/Scripts/Tiles.cs
@@ -1,16 +1,46 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Tiles : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private Color _color1, _color2;
+    [SerializeField] private SpriteRenderer _spriteRenderer;
+    [SerializeField] private GameObject HighLight;
+
+    private GameObject LastHovered;
+    public void SetColor(bool isDifferent)
     {
-        
+        _spriteRenderer.color = isDifferent ? _color1 : _color2;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Vector2 mousePos = Mouse.current.position.ReadValue(); // mostra a posicao do mouse
+        Vector2 worldPos = Camera.main.ScreenToWorldPoint(mousePos); // mostra a posicao do mouse em relação a tela toda
+
+
+        RaycastHit2D hit = Physics2D.Raycast(worldPos, Vector2.zero);
+
+        if (hit.collider != null) // se tiver colisao...
+        {
+            if (LastHovered != hit.collider.gameObject)
+            {
+                if (LastHovered != null)
+                {
+                    LastHovered.transform.GetChild(0).gameObject.SetActive(false);
+                }
+                LastHovered = hit.collider.gameObject;
+                hit.collider.gameObject.transform.GetChild(0).gameObject.SetActive(true);
+            }
+        }
+        else if (LastHovered != null)
+        {
+            LastHovered.transform.GetChild(0).gameObject.SetActive(false);
+            LastHovered = null;
+        }
+
+
     }
+
 }
